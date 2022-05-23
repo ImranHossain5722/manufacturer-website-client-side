dc96bd497b8b95c1001e857aba957506dda755ed
ImranHossain5722 <imd572258@gmail.com>
2022-05-23 17:50:58 +0600

    build dashboard

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import NotFound from '../src/Shared/NotFound/NotFound';
 import Footer from './Shared/Footer/Footer';
 import Blogs from './pages/Blogs/Blogs';
 import RequireAuth from './Shared/RequireAuth/RequireAuth';
+import AddReview from './pages/Dashboard/AddReview/AddReview';
+import MyProfile from './pages/Dashboard/MyProfile/MyProfile';
+import MyOrders from './pages/Dashboard/MyOrders/MyOrders';
 
 function App() {
   return (
@@ -20,7 +23,10 @@ function App() {
           <Route path='/portfolio' element={<MyPortfolio></MyPortfolio>} > </Route>
           <Route path='/dashboard' element={<RequireAuth><Dashboard></Dashboard></RequireAuth>} > 
           
-          
+          <Route index element={<MyProfile></MyProfile>}></Route>
+
+          <Route path='myorders' element={<MyOrders></MyOrders>}></Route>
+          <Route path='addreview' element={<AddReview></AddReview>}></Route>
           
           </Route>
           <Route path='/blogs' element={<Blogs></Blogs>} > </Route>
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
+  return (
+    <div>
+            <div class="drawer drawer-mobile">
+        <input id="dashboardSidebar" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content ">
+          <h2 className="text-4xl text-primary text-center p-4 font-bold uppercase">
+            Welcome to your Dashboard
+          </h2>
+          <Outlet />
         </div>
-    );
+        <div class="drawer-side">
+          <label for="dashboardSidebar" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
+            <li>
+              <Link to="/dashboard">My Profile</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/myorders">My Orders</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/addreview">Add a review</Link>
+            </li>
+            {/* { 
+             admin && <>
+             <li> <Link to="/dashboard/users">All Users</Link></li>
+
+             <li> <Link to="/dashboard/addDoctor">ADD Doctor</Link></li>
+            
+             <li> <Link to="/dashboard/manageDoctor">Manage Doctor</Link></li>
+    
+          
+            
+             </>} */}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
